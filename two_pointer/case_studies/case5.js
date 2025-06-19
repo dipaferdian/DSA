4ca6cb71016e3a96742a0336eb0a88671f5031fb
dipaferdian <dipaferdian2@gmail.com>
2025-06-19 17:02:04 +0700

    add case 5

---
+++ two_pointer/case_studies/case5.js
@@ -0,0 +1,22 @@
+// Problem: Valid Palindrome
+// Time: coming soon 
+// Space: coming soon
+
+function case5(s){
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+  let left = 0
+  let right = s.length - 1
+
+  while(left < right){
+    if(s[left] != s[right]){
+      return false
+    }else{
+      left++
+      right--
+    }
+  }
+  return true
+}
+
+console.log(case5("madam"))
